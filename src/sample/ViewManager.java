81b82d963778db8603881fda5f8bacad803705b5
lucsparidans <lucsnadiraps@gmail.com>
2018-12-30 17:56:25 +0100

    Implementation

--- src/sample/ViewManager.java
+++ src/sample/ViewManager.java
@@ -7,6 +7,19 @@ import java.util.Stack;
 public class ViewManager {
     private Stack<Stage> viewStack;
     public ViewManager(Stage stage){
+        this.viewStack = new Stack<>();
         viewStack.push(stage);
+    }
+    public void push(Stage s){
+        if(!viewStack.isEmpty()){
+            viewStack.peek().hide();
+        }
+        if (s != null){
+            this.viewStack.push(s);
+        }
+    }
+    public void render(){
+
+        viewStack.peek().show();
     }
 }
